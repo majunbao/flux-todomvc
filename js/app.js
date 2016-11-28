f38562d3480a213734ceb765d5f80ed962226298
ma junbao <mjbwork12@gmail.com>
2016-11-28 23:01:33 +0800

    init

--- js/app.js
+++ js/app.js
@@ -1,6 +1,35 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
 
-React.render(
-  <Test />,
+
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="Sara"></Welcome>
+      <Welcome name="Cahal"></Welcome>
+      <Welcome name="Edite"></Welcome>
+    </div>
+  )
+}
+const element = <Welcome name="Sara" />;
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      
+      <div className="Comment-date">
+        {props.date}
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <Comment date="222"/>,
   document.getElementById('todoapp')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './js/app.js',
   output: {
     path: path.resolve(__dirname, 'js'),
-    filename: 'bundle.js'
+    filename: 'bundle.min.js'
   },
   module: {
     rules: [
@@ -16,5 +17,23 @@ module.exports = {
         }
       }
     ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  // resolve: {
+  //   alias: {
+  //     'react': 'react/dist/react.min.js',
+  //     'react-dom': 'react-dom/dist/react-dom.min.js'
+  //   }
+  // },
+  devServer: {
+    // contentBase: '.',
+    inline: true
   }
 }
